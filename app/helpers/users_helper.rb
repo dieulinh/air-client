38aa6e078796113a52a386a6ac15b077d1d70070
Linh Nguyen <linhvn09@gmail.com>
2015-12-01 23:01:24 +0700

    allow facebook authenticate in views

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,4 +3,7 @@ module UsersHelper
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
   end
+  def omniauth_authorize_path(resource_name, provider)
+    send "#{resource_name}_omniauth_authorize_path", provider
+  end
 end
