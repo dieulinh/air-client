54764ecf2950e6c16029c6da5486c4fdb76f5f00
Linh Nguyen <linhvn09@gmail.com>
2015-12-02 11:36:44 +0700

    show user information

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,19 +5,16 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable, :omniauth_providers => [:facebook]
   validates :fullname, presence: true, length: {maximum: 50}
   def self.from_omniauth(auth)
-    user = User.where(email:  auth.info.email).first
-    if user
-      return user
-    else
-      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-        user.email = auth.info.email
-        user.image = auth.info.image
-        user.provider = auth.provider
-        user.uid = auth.uid
-        user.fullname = auth.info.name
-        user.password = Devise.friendly_token[0, 20]
-        user.save
-      end
+    user = User.find_by_email(auth.info.email)
+    return user if user
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.image = auth.info.image
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.fullname = auth.info.name
+      user.password = Devise.friendly_token[0, 20]
+      user.save
     end
   end
 end
