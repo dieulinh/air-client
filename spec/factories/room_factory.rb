13513695a051673cd55e7d5e0c580d5e206adf13
Linh Nguyen <linhvn09@gmail.com>
2015-12-02 15:05:30 +0700

    create sample data for testing

---
+++ spec/factories/room_factory.rb
@@ -0,0 +1,16 @@
+FactoryGirl.define do
+  factory :room do
+    home_type 'House'
+    room_type 'Entire'
+    bed_room 2
+    bath_room 2
+    accommodate 4
+    active true
+    summary "This room has a nice view nearby SG river"
+    listing_name "it good for a small family"
+    is_tv false
+    is_heating true
+    price 250
+    address "92 Nguyen Huu Canh"
+  end
+end
