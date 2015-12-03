ae3001ceb58ec24264528e9d98bd00615c6042bb
Linh Nguyen <linhvn09@gmail.com>
2015-12-03 14:34:29 +0700

    add longtitude and latitude for address to use geocoder gem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151202111445) do
+ActiveRecord::Schema.define(version: 20151203070223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 20151202111445) do
     t.integer  "user_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.float    "latitude"
+    t.float    "longtitude"
   end
 
   add_index "rooms", ["user_id"], name: "index_rooms_on_user_id", using: :btree
