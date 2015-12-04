92bbf4d4ff4294f655000175f5463b0a17c08a35
Linh Nguyen <linhvn09@gmail.com>
2015-12-04 09:34:45 +0700

    create view for reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,7 +2,7 @@ class ReservationsController < ApplicationController
   before_action :authenticate_user!
   def create
     @reservation = current_user.reservations.create(reservation_params)
-    redirect_to @reservation.room
+    redirect_to @reservation.room, notice: "Your booking has been made. Thank you for using our service"
   end
 
   private
