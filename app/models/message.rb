1b4b4c414400fbe526c18657b5e00700e8dbc0f1
Linh Nguyen <linhvn09@gmail.com>
2015-12-07 21:28:15 +0700

    restyle code

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,9 @@ class Message < ActiveRecord::Base
   belongs_to :conversation, dependent: :destroy
   belongs_to :user
 
-  validates_presence_of :content, :user_id, :conversation_id
+  validates :content, presence: true
+  validates :user_id, presence: true
+  validates :conversation_id, presence: true
 
   def sent_time
     created_at.strftime("%m/%e/%y %H:%M")
