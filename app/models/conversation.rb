0ef9be003c01d51b6af762b9761f1d59f95e3070
Linh Nguyen <linhvn09@gmail.com>
2015-12-07 15:02:36 +0700

    create association between conversation and message

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,6 +1,7 @@
 class Conversation < ActiveRecord::Base
   belongs_to :sender, foreign_key: :sender_id, class_name: 'User'
   belongs_to :recipient, foreign_key: :recipient_id, class_name: 'User'
+  has_many :messages, dependent: :destroy
 
   validates :sender_id, uniqueness: {scope: :recipient_id}
 
