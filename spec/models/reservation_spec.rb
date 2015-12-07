935023847f6ddd3c420220eab28a2f0ca8ebb127
Linh Nguyen <linhvn09@gmail.com>
2015-12-07 10:43:24 +0700

    update test case to include both context with end_date field

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -4,12 +4,23 @@ RSpec.describe Reservation, type: :model do
 
   it { should validate_presence_of :start_date }
   it { should validate_presence_of :end_date }
-  describe 'should have start_date is less than or equal end_date' do
-    let(:reservation) { FactoryGirl.build(:reservation, end_date: Time.zone.yesterday) }
 
-    it "show errors message" do
-      reservation.validate
-      expect(reservation.errors[:end_date]).to include("End date should greater than Start Date")
+  describe 'should have start_date is less than or equal end_date' do
+
+    context "When user input start_date greater than end_date" do
+      let(:reservation) { FactoryGirl.build(:reservation, start_date: Time.zone.today, end_date: Time.zone.yesterday) }
+      it "show errors message" do
+        reservation.validate
+        expect(reservation.errors[:end_date]).to include("End date should greater than Start Date")
+      end
     end
+    context "When user input valid end_date" do
+      let(:reservation) { FactoryGirl.build(:reservation, start_date: Time.zone.today, end_date: Time.zone.tomorrow) }
+      it "not contain errors message on end_date field" do
+        reservation.validate
+        expect(reservation.errors[:end_date]).to_not include("End date should greater than Start Date")
+      end
+    end
+
   end
 end
