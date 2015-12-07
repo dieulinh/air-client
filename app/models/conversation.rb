24f4834f4d61e971a2ed574f8a1ba80aed061f0b
Linh Nguyen <linhvn09@gmail.com>
2015-12-07 14:33:35 +0700

    restyle code

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -2,7 +2,7 @@ class Conversation < ActiveRecord::Base
   belongs_to :sender, foreign_key: :sender_id, class_name: 'User'
   belongs_to :recipient, foreign_key: :recipient_id, class_name: 'User'
 
-  validates_uniqueness_of :sender_id, scope: :recipient_id
+  validates :sender_id, uniqueness: {scope: :recipient_id}
 
   scope :involving, -> (user) {where("sender_id = ? OR recipient_id = ?", user.id, user.id)}
 
