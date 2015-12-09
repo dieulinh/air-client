02574bb4cc460e7f758033d66a12233c43e6990f
Linh Nguyen <linhvn09@gmail.com>
2015-12-09 15:17:05 +0700

    implement average_rating for for counting stars of reviews

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -9,4 +9,15 @@ RSpec.describe Room, type: :model do
   it { should validate_presence_of :address }
   it { should validate_length_of(:listing_name).is_at_most(100) }
   it { should validate_length_of(:summary).is_at_most(500) }
+
+  describe '#average_rating' do
+    let!(:room) { FactoryGirl.create(:room) }
+    let!(:user1) { FactoryGirl.create(:user, email: "user1@example.com") }
+    let!(:user2) { FactoryGirl.create(:user, email: "user2@example.com") }
+    let!(:review1) { FactoryGirl.create(:review, room: room, user: user1, star: 4) }
+    let!(:review2) { FactoryGirl.create(:review, room: room, user: user2, star: 5) }
+    it "should show the correct average value of rating" do
+      expect(room.average_rating).to eq 4.5
+    end
+  end
 end
