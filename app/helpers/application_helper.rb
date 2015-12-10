46217a10bdd070dd71afe32ba78c2d7228d6017a
Linh Nguyen <linhvn09@gmail.com>
2015-12-10 15:40:13 +0700

    using helper to find thumb for a room

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def room_url(room)
+    if room.photos.present?
+      room.photos.first.image.url(:thumb)
+    else
+      image_path("sample-room.jpg")
+    end
+  end
 end
