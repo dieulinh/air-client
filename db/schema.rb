e8e619bf909d421fa8fa06ca6d33f51dff05bce3
Linh Nguyen <linhvn09@gmail.com>
2015-12-10 09:33:11 +0700

    add status column to reservation db table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151209064601) do
+ActiveRecord::Schema.define(version: 20151210022635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20151209064601) do
     t.integer  "total"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "status"
   end
 
   add_index "reservations", ["room_id"], name: "index_reservations_on_room_id", using: :btree
