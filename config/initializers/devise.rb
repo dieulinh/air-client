eb557003ac14b1f348324ca44f86fcef15165490
Linh Nguyen <linhvn09@gmail.com>
2015-12-11 16:21:12 +0700

    change email sender

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'Linh Nguyen <elmido.it@gmail.com>'
+  config.mailer_sender = 'Linh Nguyen <linh@mortgageclub.co>'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
