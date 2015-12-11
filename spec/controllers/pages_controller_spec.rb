58d3fa52c8e98f6c9495c204048274ddbf3155f1
Linh Nguyen <linhvn09@gmail.com>
2015-12-11 17:43:26 +0700

    write test case for search function

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+RSpec.describe PagesController, type: :controller do
+  describe "GET #search" do
+    let!(:room1) { FactoryGirl.create(:room, address: "A28, 216 Nguyễn Văn Hưởng (Hẻm 2), Thảo Điền, Hồ Chí Minh, Vietnam") }
+    let!(:room2) { FactoryGirl.create(:room, address: "195 Nguyen Van Huong Street, Thao Dien Ward, District 2 (195 Nguyễn Văn Hưởng, Phường Thảo Điền, Quận 2), Vietnam") }
+    it "get all records match the key words" do
+
+      get :search, q: { search: "Nguyễn Văn Hưởng (Hẻm 2), Thảo Điền, Hồ Chí Minh, Vietnam"}
+      expect(assigns(:arr_rooms).size).to eq 2
+    end
+  end
+end
